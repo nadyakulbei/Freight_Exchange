0ed997cda624f4522656229b5d9f2168a2139b6f
Nadya Kulbei <6886437nadik@gmail.com>
2022-12-22 22:13:13 +0300

    Добавила нумерацию страницы ( отображение списка грузов)

--- logistics_news/logapps/views.py
+++ logistics_news/logapps/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Loads
 from .forms import LoadsForm
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def index(request):
     load = Loads.objects.all()
-    return render(request, 'logapps/index.html', {'title': "Основная страница", 'loads': load})
+    paginator = Paginator(load, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'logapps/index.html', {'page_obj': page_obj,'title': "Основная страница", 'loads': load})
 
 
 def about(request):
